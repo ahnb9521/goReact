7690ad253acae320a6ac29dab11ab50d04ec085a
Master <ahnb21@gmail.com>
2020-06-01 17:25:39 +0900

    네이버 영화 api 읽어오기

--- server/main.go
+++ server/main.go
@@ -45,21 +45,22 @@ func movieSearch(c echo.Context) error {
 		RawQuery: v.Encode(),
 	}
 
-	req, err := http.NewRequest("GET", u.String(), nil)
+	req, err := http.NewRequest("GET", u.String(), nil) //background context를 이용해 request를 만든다.
+
+	fmt.Println("Req >>>> ", req)
 
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("X-Naver-Client-Id", "Tbe5UQm8vgGeeQwbfj_L")
 	req.Header.Set("X-Naver-Client-Secret", "5lb5XYyNV1")
 
 	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := client.Do(req) //클라이언트가 req에 담긴 정보로 요청하고 응답을 resp에 담는다.
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 	defer resp.Body.Close()
 	data, err := ioutil.ReadAll(resp.Body)
-	fmt.Println("u >>>>> " + u.String()) // 넘길 주소
-	fmt.Println("============================")
-	fmt.Println("resp >>>>>> ", resp)
+	fmt.Println("data >>>> ", string(data))
 	return c.String(http.StatusOK, string(data))
 }
